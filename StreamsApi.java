f07951f591b9648874f81f57bb78ac6149d1ee23
Deepak Singh <deepaksingh.b@gmail.com>
2024-03-06 12:05:59 +0530

    Added spring templates

--- StreamsApi.java
+++ StreamsApi.java
@@ -1,5 +1,15 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamsApi {
     public static void main(String[] args) {
-        forEach()
+        List<String> names = List.of("Deepak","Abha","Deeksha","Anchal","Gautam");
+
+        List<String> sortedNames = names.stream().filter(a -> a.startsWith("A")).collect(Collectors.toList());
+        System.out.println(sortedNames);
+        System.out.println(names.stream().sorted((a,b) -> a.compareTo(b)).collect(Collectors.toList()));
+        List<String> newname = List.of("One","Two", "Three", "Four","Five");
+        System.out.println(newname);
+        
     }
 }
